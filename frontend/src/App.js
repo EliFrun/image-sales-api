0eaccfe55d1bffc4395a03f6c64c7bcd7b0e8a5b
Eli Frungorts <eli.frungorts@gmail.com>
2022-01-06 18:39:18 -0500

    added some button rendering logic for front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
 
 function App() {
+  const [viewProducts, setViewProducts] = useState(true);
   return (
     <div className="App">
       <header className="App-header">Shopify Challenge</header>
+      <div className="Buttons">
+        <Button className='button1' variant='primary' title="View Products">View Products</Button>{' '}
+        <Button className='button2' variant='primary' title="Add Products">Add Products</Button>{' '}
+      </div>
+      {viewProducts ? <div>foo</div> : <div>bar</div>}
     </div>
+
   );
 }
 
